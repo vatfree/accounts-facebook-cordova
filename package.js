c614a87f98762e3267950ef9d7806e5326840536
Jaco Swarts <jaco@vatfree.com>
2023-04-05 12:29:18 +0200

    Bump Meteor version support

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
 Package.describe({
   summary: "Login service for Facebook (2.4) accounts (works with cordova)",
-  version: "0.0.10",
-  git: "https://github.com/sajhu/accounts-facebook.git",
+  version: "0.0.11",
+  git: "https://github.com/vatfree/accounts-facebook-cordova.git",
   author: "Santiago Rojas",
-  name: "sajhu:accounts-facebook-cordova",
+  name: "vatfree:accounts-facebook-cordova",
 });
 
-Package.on_use(function(api) {
-  api.versionsFrom('1.2.0.1');
+Package.onUse(function(api) {
+  api.versionsFrom('2.3');
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('facebook-oauth@1.3.1', ['client', 'server']);
+  api.use('facebook-oauth', ['client', 'server']);
 
   api.use('service-configuration', ['client', 'server']);
   api.use('http', ['server']);
   api.use('underscore', 'server');
 
-  api.add_files('facebook_server.js', 'server');
-  api.add_files("facebook.js");
+  api.addFiles('facebook_server.js', 'server');
+  api.addFiles("facebook.js");
 });
